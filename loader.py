57e973e1d46e10252c001d3ccb85d8063bdec979
Fabio Hernan Gomez Rubio <fabiogomez11c@gmail.com>
2023-11-10 14:34:11 -0500

    Add EmbedChunk class for HuggingFace embeddings

--- loader.py
+++ loader.py
@@ -41,4 +41,31 @@ new_lengths = [len(doc.page_content) for doc in documents]
 # plt.show()
 
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.embeddings.huggingface import HuggingFaceEmbedding
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+
+
+class EmbedChunk:
+    def __init__(self, model_name):
+        self.embedding_model = HuggingFaceEmbeddings(
+            model_name=model_name,
+            model_kwargs={"device": "cpu"},
+            encode_kwargs={
+                "device": "cpu",
+                "batch_size": 8,
+            },
+        )
+
+    def __call__(self, batch):
+        embeddings = self.embedding_model.embed_documents(batch.page_content)
+        return {
+            "text": batch.page_content,
+            "source": batch.metadata["source"],
+            "embeddings": embeddings,
+        }
+
+
+embedder = EmbedChunk("distilbert-base-uncased")
+result = embedder(documents[0])
+
+
+print("Done")
