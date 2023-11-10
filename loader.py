fe4eb416db4cf1dde8ebfd368477bbe0299e8b30
Fabio Hernan Gomez Rubio <fabiogomez11c@gmail.com>
2023-11-09 21:53:15 -0500

    progress with understanding

--- loader.py
+++ loader.py
@@ -1,18 +1,45 @@
 from langchain.document_loaders import BSHTMLLoader
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import (
+    RecursiveCharacterTextSplitter,
+)
 from langchain.vectorstores.chroma import Chroma
 from utils import SectionBSHTMLLoader
 from functools import reduce
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 # list all html files using glob
 files_path = Path("./docs.ray.io/")
-html_files = list(files_path.rglob("*.html"))[0:10]
+html_files = list(files_path.rglob("*.html"))[0:50]
 html_documents = [SectionBSHTMLLoader(html_file).load() for html_file in html_files]
 html_documents = list(reduce(lambda x, y: y + x, html_documents, []))
 
-# text_splitter = CharacterTextSplitter(chunk_size=300, chunk_overlap=0)
-# documents = text_splitter.split_documents(html_documents)
+lengths = [len(doc.page_content) for doc in html_documents]
+
+
+"""
+There are some sections with a lot of charecters, that could be an issue for the context length of the LLM.
+"""
+# plt.plot(lengths, marker="o")
+# plt.title("Section Lengths")
+# plt.ylabel("# of Characters")
+# plt.xlabel("Section Number")
+# plt.show()
+
+# The original example uses RecursiveCharacterTextSplitter, is is better to use this method to maintain the chunk size
+# plain CharacterTextSplitter could lead to chunks to big for the LLM context length
+text_splitter = RecursiveCharacterTextSplitter(
+    separators=["\n\n", "\n", " ", ""], chunk_size=300, chunk_overlap=100
+)
+documents = text_splitter.split_documents(html_documents)
+new_lengths = [len(doc.page_content) for doc in documents]
+
+# plt.plot(new_lengths, marker="o")
+# plt.title("Section Lengths")
+# plt.ylabel("# of Characters")
+# plt.xlabel("Section Number")
+# plt.show()
+
 # html_ = html_files[0]
 # document = BSHTMLLoader(html_).load()
 
