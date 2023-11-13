c661884c9ee5956bfe25ddfc4ebf33195bf693bf
Fabio Hernan Gomez Rubio <fabiogomez11c@gmail.com>
2023-11-12 20:50:27 -0500

    Refactor LLM class to remove unused argument and update method signature

--- llm.py
+++ llm.py
@@ -10,22 +10,19 @@ warnings.filterwarnings("ignore")
 class LLM:
     """LLM object designed for our endpoint"""
 
-    def __init__(self, conversation_phase: str):
-        self.conversation_phase = conversation_phase
-
-    def generate_code(self, user_html: str, user_request: str) -> str:
+    def generate_code(self, user_request: str, generated_context: str) -> str:
         """Generate the desired code based on the user input"""
         chat_model = ChatOpenAI()
         chat_prompt = ChatPromptTemplate.from_messages(
             [("system", base_template), ("human", base_human_template)]
         )
         chain = LLMChain(prompt=chat_prompt, llm=chat_model)
-        result = chain.run({"html": user_html, "request": user_request})
+        result = chain.run()
         return result
 
 
 if __name__ == "__main__":
-    llm = LLM(conversation_phase="none")
+    llm = LLM()
     res = llm.generate_code(
         user_html="<button>Click me!</button>",
         user_request="I want this button to have light colors and a hover effect, make it whole circle button, away from the standards even if you need to make it bigger to be a circle.",
--- templates.py
+++ templates.py
@@ -1,27 +1,11 @@
 base_template = """
-You are an AI designer assistant and I'm a human who knows how to code but has a bad taste, I need to give you code and you'll return me the code with the requests that I make and good looking, I want to end up with beautiful things.
-All the code that you'll return me will be using TailwindCSS, so I'll give you the HTML and you'll give me the HTML with the correct classNames using the TailwindCSS convention.
-Just to be clear, you just need to answer me with the code, I don't want you to write me anything else, JUST THE CODE, ONLY THE CODE.
-So the format of my input will be:
-[HTML code]
-[UI request]
-For example:
-[HTML code]
-```html
-<button>Hello world!</button>
-```
-[UI request]
-Make this button similar to what Material UI philosophy is, I want this button to be good looking, consider dark colors in the background.
-
-Your output should be something like:
-```html
-<button class="border shadow">Hellow world!</button>
-```
+You are an expert in Ray, which is open source framework to scale AI applications. It provides the compute layer for parallel processing so that the user does not need to be a distributed systems expert.
+The user will give you a request and you will have to answer to fulfill the request, also an additional context will be provided to you in order to have more information to answer the request.
 """
 
 base_human_template = """
-[HTML code]
-{html}
-[UI request]
+User request:
 {request}
+This is the most related content to the user request:
+{context}
 """
