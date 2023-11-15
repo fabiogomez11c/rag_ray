3b21db72b56857a70f6aca624ac220f934aeb1ec
Fabio Hernan Gomez Rubio <fabiogomez11c@gmail.com>
2023-11-15 07:24:37 -0500

    Add OpenAI text embeddings support to EmbedChunk class

--- embedder.py
+++ embedder.py
@@ -1,17 +1,21 @@
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from langchain.embeddings import OpenAIEmbeddings
 from typing import Dict
 
 
 class EmbedChunk:
     def __init__(self, model_name):
-        self.embedding_model = HuggingFaceEmbeddings(
-            model_name=model_name,
-            # model_kwargs={"device": "cpu"},
-            # encode_kwargs={
-            #     "device": "cpu",
-            #     "batch_size": 8,
-            # },
-        )
+        if model_name == "text-embedding-ada-002":
+            self.embedding_model = OpenAIEmbeddings(model=model_name)
+        else:
+            self.embedding_model = HuggingFaceEmbeddings(
+                model_name=model_name,
+                # model_kwargs={"device": "cpu"},
+                # encode_kwargs={
+                #     "device": "cpu",
+                #     "batch_size": 8,
+                # },
+            )
 
     def __call__(self, batch) -> Dict:
         embeddings = self.embedding_model.embed_documents([batch.page_content])
